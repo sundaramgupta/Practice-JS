f76ad5df458dd37a7f298b0234c025256bf0afcc
Sundaram Gupta <sundaramgupta18@gmail.com>
2020-05-31 14:24:56 +0530

    added handlebars to generate template

---
+++ roll-a-dice/dice.js
@@ -0,0 +1,13 @@
+const template = Handlebars.compile("<li> You rolled a {{  value  }}</li>");
+
+document.addEventListener('DOMContentLoaded', () => {
+	document.querySelector('#roll').onclick = () => {
+
+		//generate a random number for the dice
+		const roll = Math.floor((Math.random()*6) +1);
+
+		//to add roll to DOM
+		const content = template({'value' : roll});
+		document.querySelector('#rolls').innerHTML += content;
+	};
+});
